4b37d03bc838789d9787098030af0f81878dca35
luan2003bgdz <luan2003bgdz@gmail.com>
2024-07-04 18:27:39 +0700

    update

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h2>Order your favorite food here</h2>
+        <p>Pho is essentially Vietnams signature dish, comprising rice noodles in a flavourful soup , meat and various greens, plus a side of nuoc cham fermented fish or chilli, A basic bowl contains beef or chicken, topped, bean sprouts, lime wedges, and fresh herbs s</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
